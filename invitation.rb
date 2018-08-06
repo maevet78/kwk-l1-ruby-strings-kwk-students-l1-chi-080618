1cadd932576df3f7d5216dc8df6dec37cde58ed5
maevet78 <190439mt@saintviator.com>
2018-08-06 21:13:50 +0000

    Done.

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,13 @@
-# Code your prompts here!
+guest_name = "Maeve"
+party_name = "Met Gala"
+date = "May 7th"
+time = "7 pm"
+host_name = "The Metropolitan Museum of Art"
 
-# Try starting out with puts'ing a string.
+puts "Dear #{guest_name},"
+puts " "
+puts "You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than January 30."
+puts " "
+puts "Sincerely,"
+puts " #{host_name}"
+
